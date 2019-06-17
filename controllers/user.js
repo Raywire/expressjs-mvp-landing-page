e54af1450c35a93dacbd76c83bfbeb669d1aa89e
Ryan Wire Simiyu <raywire@Ryans-MacBook-Pro.local>
2019-06-17 21:00:06 +0300

    Implemented error handling and form validation for signup route

--- controllers/user.js
+++ controllers/user.js
@@ -3,6 +3,9 @@ let bycrpt = require('bcrypt');
 const passport = require('passport');
 const myPassport = require('../passport_setup')(passport);
 let flash = require('connect-flash');
+const { isEmpty } = require('lodash');
+
+const { validateUser } = require('../validators/signup');
 
 exports.show_login = function(req, res, next) {
   res.render('user/login', { formData: {}, errors: {} });
@@ -10,6 +13,10 @@ exports.show_login = function(req, res, next) {
 
 exports.show_signup = function(req, res, next) {
   res.render('user/signup', { formData: {}, errors: {} });
+}
+
+const rerender_signup = function(errors, req, res, next) {
+  res.render('user/signup', { formData: req.body, errors: errors });
 }
 
 exports.login = function(req, res, next) {
@@ -24,16 +31,23 @@ const generateHash = function(password) {
   return bycrpt.hashSync(password, bycrpt.genSaltSync(8), null)
 }
 exports.signup = function(req, res, next) {
-  const newUser = models.User.build({
-    email: req.body.email,
-    password: generateHash(req.body.password)
-  });
-  return newUser.save().then(result => {
-    passport.authenticate('local', {
-      successRedirect: "/",
-      failureRedirect: "/signup",
-      failureFlash: true
-    })(req, res, next);
+  let errors = {};
+  return validateUser(errors, req).then(errors => {
+    if(!isEmpty(errors)) {
+      rerender_signup(errors, req, res, next);
+    } else {
+      const newUser = models.User.build({
+        email: req.body.email,
+        password: generateHash(req.body.password)
+      });
+      return newUser.save().then(result => {
+        passport.authenticate('local', {
+          successRedirect: "/",
+          failureRedirect: "/signup",
+          failureFlash: true
+        })(req, res, next);
+      })
+    }
   })
 }
 
