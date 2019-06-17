df088dfcf3693328a1e3b159029bfe5ee757d098
Ryan Wire Simiyu <raywire@Ryans-MacBook-Pro.local>
2019-06-17 21:52:44 +0300

    Add flash to app.js to avoid that flash is not recognized

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ let session = require('express-session');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+let flash = require('connect-flash')
 require('./passport_setup')(passport);
 
 var app = express();
@@ -17,6 +18,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(flash())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
